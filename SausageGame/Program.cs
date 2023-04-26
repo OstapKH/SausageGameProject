b4bc9a88b59eb19dfa89b24e7b1d21aae281d1d5
Vitalik2705 <vitok2misze@gmail.com>
2023-04-26 13:10:06 +0300

    Added final impl of SausageGame

--- SausageGame/Program.cs
+++ SausageGame/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+namespace SausageGame
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new Game(new List<Player> { new Player("Petro"), new Player("Mariyka"), new Player("Vitaliy"),  new Player("Serhiy") },
+                52);
+            game.DealCards(52);
+
+            List<Player> players = game.Players;
+
+            foreach (Player player in players)
+            {
+                Console.WriteLine(player);
+            }
+            
+            game.StartGame();
+        }
+    }
+}
